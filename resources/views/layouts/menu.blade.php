9110c723f5b8165c44fd935a4631fd229c0128fa
Anthony Ricardo Thiotanry <belzeharkxarus@ymail.com>
2018-06-04 10:37:06 +0700

    Different menu on Authentication

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,3 +1,45 @@
+@if (!Auth::check())
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('home') }}">
+            <img src="images/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+            Dorah
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarControls" aria-controls="navbarControls" aria-expanded="false" aria-label="Buka Navigasi">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarControls">
+            <ul class="navbar-nav mr-auto">
+                @if ($route == 'home')
+                <li class="nav-item active">
+                @else
+                <li class="nav-item">
+                @endif
+                    <a class="nav-link" href="{{ route('home') }}">Beranda</a>
+                </li>
+                @if ($route == 'event')
+                <li class="nav-item active">
+                @else
+                <li class="nav-item">
+                @endif
+                    <a class="nav-link" href="{{ route('event') }}">Event</a>
+                </li>
+                @if ($route == 'help')
+                <li class="nav-item active">
+                @else
+                <li class="nav-item">
+                @endif
+                    <a class="nav-link" href="{{ route('help') }}">Bantuan</a>
+                </li>
+            </ul>
+            <div class="form-inline">
+                <a href="{{ route('login') }}" class="btn btn-light mr-sm-2">Masuk</a>
+                <a href="{{ route('register') }}" class="btn btn-danger mr-sm-1">Daftar</a>
+            </div>
+        </div>
+    </div>
+</nav>
+@else
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
     <div class="container">
         <a class="navbar-brand" href="/">
@@ -9,20 +51,32 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarControls">
             <ul class="navbar-nav mr-auto">
+                @if ($route == 'home')
                 <li class="nav-item active">
-                    <a class="nav-link" href="/">Beranda</a>
-                </li>
+                @else
                 <li class="nav-item">
-                    <a class="nav-link" href="/event">Event</a>
+                @endif
+                    <a class="nav-link" href="{{ route('home') }}">Beranda</a>
                 </li>
+                @if ($route == 'event')
+                <li class="nav-item active">
+                @else
                 <li class="nav-item">
-                    <a class="nav-link" href="/help">Bantuan</a>
+                @endif
+                    <a class="nav-link" href="{{ route('event') }}">Event</a>
+                </li>
+                @if ($route == 'help')
+                <li class="nav-item active">
+                @else
+                <li class="nav-item">
+                @endif
+                    <a class="nav-link" href="{{ route('help') }}">Bantuan</a>
                 </li>
             </ul>
-            <div class="form-inline">
-                <a href="/login" class="btn btn-light mr-sm-2">Masuk</a>
-                <a href="/register" class="btn btn-danger mr-sm-1">Daftar</a>
-            </div>
+            <!-- <div class="form-inline">
+                <a href="" class="btn btn-danger mr-sm-1">Logout</a>
+            </div> -->
         </div>
     </div>
 </nav>
+@endif
