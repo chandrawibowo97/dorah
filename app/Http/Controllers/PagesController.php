6b8d06aa80a742b1c735e1433435495f0d05d82c
Anthony Ricardo Thiotanry <belzeharkxarus@ymail.com>
2018-06-08 10:38:18 +0700

    Dynamic map data based on database

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 class PagesController extends Controller
 {
@@ -22,7 +23,7 @@ class PagesController extends Controller
     public function map()
     {
         $route = Route::currentRouteName();
-        
+
         $data = array(
             'title' => 'Peta Darah - Dorah',
             'route' => $route
@@ -46,6 +47,15 @@ class PagesController extends Controller
     { //halaman index setelah login
         $user = Auth::user();
         $route = Route::currentRouteName();
+        $branches = DB::table('pmi_branches')
+            ->join('districts', 'pmi_branches.district_id', '=', 'districts.id')
+            ->join('cities', 'districts.city_id', '=', 'cities.id')
+            ->join('provinces', 'cities.province_id', '=', 'provinces.id')
+            ->select('pmi_branches.*', 'districts.name as district', 'cities.name as city', 'provinces.name as province')
+            ->get();
+        $stocks = DB::table('blood_stocks')
+            ->join('blood_types', 'blood_stocks.blood_type_id', '=', 'blood_types.id')
+            ->get();
 
         if (is_null($user)) {
             $name = '';
@@ -53,10 +63,13 @@ class PagesController extends Controller
             $name = $user->name;
         }
 
+
         $data = array(
             'title' => 'Dashboard - Dorah',
             'route' => $route,
-            'name' => $name
+            'name' => $name,
+            'branches' => $branches,
+            'stocks' => $stocks
         );
 
         return view('pages.home')->with($data);
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -114,213 +114,6 @@
             </div>
             <div class="col col-md-6">
                 <canvas id="pie_chart"></canvas>
-
-        <script type="text/javascript">
-            var myPieChart;
-            var chartWidth = 30; chartHeight= 30;
-            function initChart(data){
-
-                var pie_chart = document.getElementById('pie_chart');
-                pie_chart.width = chartWidth;
-                pie_chart.height = chartHeight;
-                var ctx_pie = pie_chart.getContext("2d");
-                // destroy last created chart
-                if(myPieChart != undefined){
-                    myPieChart.destroy();               
-                }             
-                myPieChart = new Chart(ctx_pie,{
-                    type: 'pie',
-                    data: data,
-                });
-            }
-
-        </script>
-
-        <script type="text/javascript">
-            var marker_icon = {
-                HIGH: 'http://www.millawi.com.au/wp-content/uploads/2016/02/img_Circle_Green.jpg',
-                LOW: 'http://www.millawi.com.au/wp-content/uploads/2016/02/img_Circle_Red.jpg',
-                DEFAULT: 'http://www.millawi.com.au/wp-content/uploads/2016/02/img_Circle_Blue.jpg'
-            }
-
-            function getStock(branch_id){
-                for(let j = 0; j < stock.length; j++) {
-                    if(stock[j].branch_id == branch_id){
-                        return stock[j];
-                    }
-                }
-                return undefined;
-            }
-
-            function initMap() {
-                var map = new google.maps.Map(document.getElementById('maps'), {
-                  zoom: 14,
-                  center: {lat: branchs[0].lat, lng : branchs[0].lng }
-                });
-
-                var infowindow = new google.maps.InfoWindow();
-                var marker, i;
-
-                for (i = 0; i < branchs.length; i++) { 
-                    let stock = getStock(branchs[i].id) != undefined ? getStock(branchs[i].id) : undefined;
-                    marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(branchs[i].lat, branchs[i].lng),
-                        icon: {
-                            url: stock != undefined ? marker_icon[stock.alert] : marker_icon['DEFAULT'],
-                            scaledSize: new google.maps.Size(20, 20)
-                        },
-                        map: map
-                    });
-
-                  google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                    let address = branchs[i].address+', '+branchs[i].district+', '+branchs[i].city+', '+branchs[i].province+' kode pos '+branchs[i].postcode;
-
-                    return function() {
-                        let stock_darah = stock.stock_darah;                    
-                        let labels = [], stocks = [];
-
-                        stock_darah.forEach((item)=>{
-                            labels.push(item.label);
-                            stocks.push(item.stock);
-                        })
-
-                        initChart({
-                            datasets: [{
-                                data: stocks,
-                                backgroundColor: ['#f00','#0f0','#00f','#ff0','#0ff','#f0f','#000','#fff']
-                            }],
-
-                            // These labels appear in the legend and in the tooltips when hovering different arcs
-                            labels: labels
-                        });
-
-                      infowindow.setContent(address);
-                      infowindow.open(map, marker);
-                    }
-                  })(marker, i));
-                }            
-            }
-        </script>
-        <script type="text/javascript">
-            var branchs = [
-                {
-                    id: 1,
-                    name: 'UTD PMI Kota Medan',
-                    address: 'Jl. Perintis Kemerdekaan No.37',
-                    district: 'Medan Timur',
-                    city: 'Medan',
-                    province: 'Sumatera Utara',
-                    postcode: 20233,
-                    lat: 3.5992310,
-                    lng: 98.6835840
-                },
-                {
-                    id: 2,
-                    name: 'Palang Merah Indonesia',
-                    address: 'Jl. Palang Merah No.17, A U R',
-                    district: 'Medan Maimun',
-                    city: 'Medan',
-                    province: 'Sumatera Utara',
-                    postcode: 20151,
-                    lat: 3.5843472,
-                    lng: 98.6801561
-                }
-            ];
-            var stock = [
-                {
-                    branch_id: 1,
-                    alert : 'HIGH',
-                    stock_darah: [
-                        {
-                            id: 1,
-                            label: 'A+',
-                            stock: 50
-                        },
-                        {
-                            id: 2,
-                            label: 'A-',
-                            stock: 50
-                        },
-                        {
-                            id: 3,
-                            label: 'B+',
-                            stock: 50
-                        },
-                        {
-                            id: 4,
-                            label: 'B-',
-                            stock: 50
-                        },
-                        {
-                            id: 5,
-                            label: 'AB+',
-                            stock: 50
-                        },
-                        {
-                            id: 6,
-                            label: 'AB-',
-                            stock: 50
-                        },
-                        {
-                            id: 7,
-                            label: 'O+',
-                            stock: 50
-                        },
-                        {
-                            id: 8,
-                            label: 'O-',
-                            stock: 50
-                        }
-                    ]
-                },
-                {
-                    branch_id: 2,
-                    alert: 'LOW',
-                    stock_darah: [
-                        {
-                            id: 9,
-                            label: 'A+',
-                            stock: 20
-                        },
-                        {
-                            id: 10,
-                            label: 'A-',
-                            stock: 15
-                        },
-                        {
-                            id: 11,
-                            label: 'B+',
-                            stock: 38
-                        },
-                        {
-                            id: 12,
-                            label: 'B-',
-                            stock: 33
-                        },
-                        {
-                            id: 13,
-                            label: 'AB+',
-                            stock: 18
-                        },
-                        {
-                            id: 14,
-                            label: 'AB-',
-                            stock: 23
-                        },
-                        {
-                            id: 15,
-                            label: 'O+',
-                            stock: 9
-                        },
-                        {
-                            id: 16,
-                            label: 'O-',
-                            stock: 11
-                        }
-                    ]
-                }
-            ];
-        </script>
             </div>
         </div>
     </div>
@@ -340,8 +133,112 @@
 
 
 @section('script')
-<script src="{{asset('js/chart.bundle.min.js')}}"></script>
-<script src="js/googlemaps.js"></script>
+<script type="text/javascript">
+    var myPieChart;
+    var chartWidth = 30; chartHeight= 30;
+    function initChart(data){
+
+        var pie_chart = document.getElementById('pie_chart');
+        pie_chart.width = chartWidth;
+        pie_chart.height = chartHeight;
+        var ctx_pie = pie_chart.getContext("2d");
+        // destroy last created chart
+        if(myPieChart != undefined){
+            myPieChart.destroy();
+        }
+        myPieChart = new Chart(ctx_pie,{
+            type: 'pie',
+            data: data,
+        });
+    }
+
+</script>
+
+<script type="text/javascript">
+    var marker_icon = {
+        HIGH: 'http://www.millawi.com.au/wp-content/uploads/2016/02/img_Circle_Green.jpg',
+        LOW: 'http://www.millawi.com.au/wp-content/uploads/2016/02/img_Circle_Red.jpg',
+        DEFAULT: 'http://www.millawi.com.au/wp-content/uploads/2016/02/img_Circle_Blue.jpg'
+    }
+
+    function getStock(branch_id){
+        let stock_list = {
+            branch_id: branch_id,
+            alert: '',
+            stock_darah: []
+        }
+        let quantity = 0;
+
+        for(let j = 0; j < stock.length; j++) {
+            if(stock[j].pmi_branch_id == branch_id){
+                stock_list.stock_darah.push(stock[j]);
+                quantity += stock[j].stock;
+            }
+        }
+
+        if (quantity >= 400) stock_list.alert = 'HIGH';
+        else stock_list.alert = 'LOW';
+
+        return stock_list;
+    }
+
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('maps'), {
+          zoom: 14,
+          center: {lat: branchs[0].lat, lng : branchs[0].lng }
+        });
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker, i;
+
+        for (i = 0; i < branchs.length; i++) {
+            let stock = getStock(branchs[i].id) != undefined ? getStock(branchs[i].id) : undefined;
+            marker = new google.maps.Marker({
+                position: new google.maps.LatLng(branchs[i].lat, branchs[i].lng),
+                icon: {
+                    url: stock != undefined ? marker_icon[stock.alert] : marker_icon['DEFAULT'],
+                    scaledSize: new google.maps.Size(20, 20)
+                },
+                map: map
+            });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            let address = branchs[i].address+', '+branchs[i].district+', '+branchs[i].city+', '+branchs[i].province+' kode pos '+branchs[i].postcode;
+            console.log(branchs[i]);
+
+            return function() {
+                let stock_darah = stock.stock_darah;
+                let labels = [], stocks = [];
+
+                stock_darah.forEach((item)=>{
+                    labels.push(item.label);
+                    stocks.push(item.stock);
+                })
+
+                initChart({
+                    datasets: [{
+                        data: stocks,
+                        backgroundColor: ['#f00','#0f0','#00f','#ff0','#0ff','#f0f','#000','#fff']
+                    }],
+
+                    // These labels appear in the legend and in the tooltips when hovering different arcs
+                    labels: labels
+                });
+
+              infowindow.setContent(address);
+              infowindow.open(map, marker);
+            }
+          })(marker, i));
+        }
+    }
+</script>
+<script type="text/javascript">
+    var branchs = {!! json_encode($branches->toArray()) !!};
+    var stock = {!! json_encode($stocks->toArray()) !!};
+</script>
+
+<script src="{{ asset('js/chart.bundle.min.js') }}"></script>
+<script src='js/googlemaps.js'></script>
 <script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
 <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBDxQBvSn1BtzShvuE3hpJZgBPQ-1MVKmw&callback=initMap"></script>
 
