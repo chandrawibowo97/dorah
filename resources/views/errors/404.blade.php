1a19ec7c2624885354d3fc7772f9feca7b6d283d
Anthony Ricardo Thiotanry <belzeharkxarus@ymail.com>
2018-06-12 11:29:34 +0700

    Customized error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.base')
+
+@section('body')
+<section class="jumbotron jumbotron-fluid text-center mb-0" style="background-color: #fff;">
+    <div class="container p-5">
+        <h1 class="display-4">404</h1>
+        <p class="lead">{{ $exception->getMessage() }}</p>
+        <p>
+            <a class="btn btn-lg btn-danger" href="{{ route('index') }}">Return to home</a>
+        </p>
+    </div>
+</section>
+@endsection
